aff530eb70637d0cacc4a3776685861065f00d5f
burakboc <55413089+burakboc@users.noreply.github.com>
2026-06-26 16:54:42 +0300

    Update cr_fins.py

--- cr_fins.py
+++ cr_fins.py
@@ -13,25 +13,11 @@ fin_j = (
 )
 
 joined = (
-    cr_rds
-    .withColumn("party_id_join", F.col("party_id").cast("decimal(16,0)"))
-    .withColumn("month_start_join", F.to_date("month_start", "yyyy-MM-dd"))
-    .alias("r")
+    cr_rds.alias("r")
     .join(
-        fin
-        .withColumn("party_id_join", F.col("party_id").cast("decimal(16,0)"))
-        .withColumn("data_date_join", F.col("data_date").cast("date"))
-        .alias("f"),
-        on=[
-            F.col("r.party_id_join") == F.col("f.party_id_join"),
-            F.col("r.month_start_join") == F.col("f.data_date_join")
-        ],
-        how="left"
-    )
-    .select(
-        "r.party_id",
-        "r.month_start",
-        "r.proposal_id",
-        *[F.col(f"f.{c}").alias(c) for c in fin.columns if c != "party_id"]
+        fin.alias("f"),
+        (F.col("r.party_id").cast("decimal(16,0)") == F.col("f.party_id")) &
+        (F.date_sub(F.to_date("r.month_start", "yyyy-MM-dd"), 1) == F.col("f.data_date")),
+        "left"
     )
 )
